d986b1a672264c8c9700e56766ec07b7417cdf25
Naive Roboticist <naiveroboticist@gmail.com>
2017-12-27 12:46:44 -0500

    Cleaning up unused stuff.

--- bin/ad-deploy-config.js
+++ bin/ad-deploy-config.js
@@ -299,7 +299,6 @@ class RoleAttrTable {
     this._roleAttrList.key(['left'], () => this._appScreen.focusToRoleList())
     this._roleAttrList.on('blur', () => this._adjustBorder('bg'))
     this._roleAttrList.on('focus', () => this._adjustBorder('line'))
-    // this._roleAttrList.key(['up', 'down'], () => this._appScreen.updateForm())
     this._roleAttrList.key('enter', () => { this._editRow() })
   }
 
@@ -322,7 +321,6 @@ class RoleAttrTable {
         }
       },
       border: {
-        // type: 'line'
       }
     })
   }
@@ -421,27 +419,6 @@ class RoleList {
 
   focus () {
     this._roleList.focus()
-  }
-}
-
-class CheckBox {
-  constructor (form, name, label, left, top) {
-    this._form = form
-    this._checkbox = this._window(form.getForm(), name, label, left, top)
-  }
-
-  _window (form, name, label, left, top) {
-    return Blessed.checkbox({
-      parent: form,
-      name: name,
-      label: label,
-      top: top,
-      left: left
-    })
-  }
-
-  check (checked) {
-    checked ? this._checkbox.check() : this._checkbox.uncheck()
   }
 }
 
@@ -545,128 +522,6 @@ class FormButton {
   _setBorderType (borderType) {
     this._button.border.type = borderType
     this._form.getAppScreen().render()
-  }
-}
-
-class InputTextField {
-  constructor (form, name, label, top) {
-    this._form = form
-    this._name = name
-    this._label = this._label(form.getForm(), label, top)
-    this._textBox = this._textBox(form.getForm(), name, label.length + 1, top)
-  }
-
-  _label (form, label, top) {
-    return Blessed.text({
-      parent: form,
-      top: top,
-      content: label
-    })
-  }
-
-  _textBox (form, name, left, top) {
-    return Blessed.textbox({
-      parent: form,
-      name: name,
-      inputOnFocus: true,
-      left: left,
-      top: top
-    })
-  }
-
-  focus () {
-    this._textBox.focus()
-  }
-
-  setContent (data) {
-    this._textBox.setValue(data)
-  }
-}
-
-class RoleForm {
-  constructor (appScreen) {
-    this._appScreen = appScreen
-    this._roleForm = this._window(appScreen.getScreen())
-    // Create all the fields we need
-    let top = 0
-    this._rpm1Field = new InputTextField(this, 'rpm1name', 'RPM 1 Name:', top++)
-    this._rpm2Field = new InputTextField(this, 'rpm2name', 'RPM 2 Name:', top++)
-    this._preMigrationScript1Field = new InputTextField(this, 'preMigrationScript1', 'PreMigration Script 1:', top++)
-    this._preMigrationScript1Duration = new DurationRadios(this, 'preMigrationScript1Duration', 0, top++)
-    top += 2
-    this._preMigrationScript2Field = new InputTextField(this, 'preMigrationScript2', 'PreMigration Script 2:', top++)
-    this._preMigrationScript3Field = new InputTextField(this, 'preMigrationScript3', 'PreMigration Script 3:', top++)
-    this._preMigrationScript4Field = new InputTextField(this, 'preMigrationScript4', 'PreMigration Script 4:', top++)
-    this._preMigrationScript5Field = new InputTextField(this, 'preMigrationScript5', 'PreMigration Script 5:', top++)
-    this._seedDataLoad = new CheckBox(this, 'seedDataLoad', '] Seed Data Load', 0, top++)
-    this._schemaMigration = new CheckBox(this, 'schemaMigration', '] Schema Migration:', 0, top++)
-    this._postMigrationScript1Field = new InputTextField(this, 'postMigrationScript1', 'PostMigration Script 1:', top++)
-    this._postMigrationScript2Field = new InputTextField(this, 'postMigrationScript2', 'PostMigration Script 2:', top++)
-    this._postMigrationScript3Field = new InputTextField(this, 'postMigrationScript3', 'PostMigration Script 3:', top++)
-    this._postMigrationScript4Field = new InputTextField(this, 'postMigrationScript4', 'PostMigration Script 4:', top++)
-    this._postMigrationScript5Field = new InputTextField(this, 'postMigrationScript5', 'PostMigration Script 5:', top++)
-    this._submitButton = new FormButton(this, 'Submit', 42, top, () => { this._submitHandler() })
-    this._cancelButton = new FormButton(this, 'Cancel', 50, top, () => { this._cancelHandler() })
-    this._roleForm.on('submit', (data) => this._submit(data))
-  }
-
-  _window (screen) {
-    return Blessed.form({
-      parent: screen,
-      left: 18,
-      top: 1,
-      width: 60,
-      height: 19,
-      keys: true,
-      border: {
-        type: 'line'
-      }
-    })
-  }
-
-  _submitHandler () {
-    this._roleForm.submit()
-  }
-
-  _cancelHandler () {
-    this._appScreen.updateForm()
-    this._appScreen.focusToRoleList()
-    this._appScreen.render()
-  }
-
-  _submit (data) {
-    this._appScreen.updateConfigDataForSelectedRole(data)
-    this._appScreen.focusToRoleList()
-  }
-
-  getForm () {
-    return this._roleForm
-  }
-
-  updateForm (roleData) {
-    this._rpm1Field.setContent(roleData.rpm1name || '')
-    this._rpm2Field.setContent(roleData.rpm2name || '')
-    this._preMigrationScript1Field.setContent(roleData.preMigrationScript1 || '')
-    this._preMigrationScript2Field.setContent(roleData.preMigrationScript2 || '')
-    this._preMigrationScript3Field.setContent(roleData.preMigrationScript3 || '')
-    this._preMigrationScript4Field.setContent(roleData.preMigrationScript4 || '')
-    this._preMigrationScript5Field.setContent(roleData.preMigrationScript5 || '')
-    this._seedDataLoad.check(roleData.seedDataLoad)
-    this._schemaMigration.check(roleData.schemaMigration)
-    this._postMigrationScript1Field.setContent(roleData.postMigrationScript1 || '')
-    this._postMigrationScript2Field.setContent(roleData.postMigrationScript2 || '')
-    this._postMigrationScript3Field.setContent(roleData.postMigrationScript3 || '')
-    this._postMigrationScript4Field.setContent(roleData.postMigrationScript4 || '')
-    this._postMigrationScript5Field.setContent(roleData.postMigrationScript5 || '')
-    this._appScreen.render()
-  }
-
-  focusOnFirstField () {
-    this._rpm1Field.focus()
-  }
-
-  getAppScreen () {
-    return this._appScreen
   }
 }
 
